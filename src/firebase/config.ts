705f0ff87fd879acf4122d1296278663d9bc857e
subrata sahu <subratasahu2327@gmail.com>
2025-11-06 08:56:47 +0000

    google signin not working showing this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "autosubscription-6c04a",
-  "appId": "1:116150082169960648349:web:3d3a0c6e3c3c1b1f3c3c1b",
-  "apiKey": "AIzaSyA2e_1o2C3b4D5e6F7g8H9i0J-k1L2m3n4o5p6",
-  "authDomain": "autosubscription-6c04a.firebaseapp.com",
+  "appId": "1:688217735729:web:d33661bd523a425e374b10",
+  "apiKey": "AIzaSyCBhtbiiVS5SsJn-N00gbM1oX625BjI3YM",
+  "authDomain": "studio-4799056812-3cb83.firebaseapp.com",
   "storageBucket": "autosubscription-6c04a.appspot.com",
-  "messagingSenderId": "116150082169960648349"
+  "messagingSenderId": "688217735729"
 };
