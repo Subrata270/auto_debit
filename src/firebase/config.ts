5850124c96e3b29e33cafb30e5aeceab9c805fd2
subrata sahu <subratasahu2327@gmail.com>
2025-11-06 09:00:09 +0000

    Fix Firebase Authentication for my “Auto-Debit Tracker Portal System.” E

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "autosubscription-6c04a",
-  "appId": "1:688217735729:web:d33661bd523a425e374b10",
   "apiKey": "AIzaSyCBhtbiiVS5SsJn-N00gbM1oX625BjI3YM",
-  "authDomain": "studio-4799056812-3cb83.firebaseapp.com",
+  "authDomain": "autosubscription-6c04a.firebaseapp.com",
+  "projectId": "autosubscription-6c04a",
   "storageBucket": "autosubscription-6c04a.appspot.com",
-  "messagingSenderId": "688217735729"
+  "messagingSenderId": "688217735729",
+  "appId": "1:688217735729:web:d33661bd523a425e374b10"
 };
