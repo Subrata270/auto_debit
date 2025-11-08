42a0f34cc3191b1255ba1eb305dd1081120b25f9
subrata sahu <subratasahu2327@gmail.com>
2025-11-08 10:35:38 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/src/app/(

--- src/app/(auth)/login/admin/page.tsx
+++ src/app/(auth)/login/admin/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import LoginForm from "@/app/components/auth/login-form";
+import LoginForm from "@/components/auth/login-form";
 
 export default function AdminLoginPage() {
     return (
